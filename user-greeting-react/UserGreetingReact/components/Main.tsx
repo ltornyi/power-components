a2b6315c8570bbbf11e830cf614ca103b5f58592
Lajos Tornyi <ltornyi@gmail.com>
2023-04-29 16:51:36 +0200

    Nicer layout with Stack

--- user-greeting-react/UserGreetingReact/components/Main.tsx
+++ user-greeting-react/UserGreetingReact/components/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Label } from '@fluentui/react';
+import { Label, Stack } from '@fluentui/react';
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
 import { SignInButton } from './SignInButton';
 import { SignOutButton } from './SignOutButton';
@@ -15,15 +15,29 @@ export const Main = (props: IMainProps) => {
   return (
     <>
       <AuthenticatedTemplate>
-        <Label>Authenticated</Label>
-        <SignOutButton />
-        <Greeting 
-          name={props.context.parameters.name.raw}
-        />
+        <Stack tokens={{padding:10, childrenGap: 5}}>
+          <Stack.Item align="center">
+            <Label>Authenticated</Label>
+          </Stack.Item>
+          <Stack.Item align="center">
+            <SignOutButton />
+          </Stack.Item>
+          <Stack.Item align="auto">
+            <Greeting 
+              name={props.context.parameters.name.raw}
+            />
+          </Stack.Item>
+        </Stack>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
-        <Label>Unauthenticated</Label>
-        <SignInButton />
+        <Stack tokens={{padding:10, childrenGap: 5}}>
+          <Stack.Item align="center">
+            <Label>Unauthenticated</Label>
+          </Stack.Item>
+          <Stack.Item align="center">
+            <SignInButton />
+          </Stack.Item>
+        </Stack>
       </UnauthenticatedTemplate>
     </>
   )
